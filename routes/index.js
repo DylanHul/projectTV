d964fd4e24b10659a1b409b717a22f8f54bf4075
Dylan <email": null>
2016-08-11 19:36:18 +1200

    handlebars and css working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {{body}}: 'My Project' });
+  res.render('index', {body: "this is my project" });
 });
 
 module.exports = router;
