a9005c97d9be39c42a0d9ccb77093955b9a73f55
Dylan <email": null>
2016-08-18 11:59:17 +1200

    displaying on page

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ var router = express.Router();
 var tvmaze = require('tvmaze-api')
 
 
-var toCall = tvmaze.getByQuery('suits', true, [], function(result){
-    return result
-});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {body: "this is my project" });
+  res.render('index', {body: "this is my project" })
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var tvmaze = require('tvmaze-api')
 
+// var toCall = tvmaze.getByQuery('suits', true, ['nextepisode'], function(result){
+//     console.log("here is the show", result)
+// })
+var input = function(){
+  return 'suits'
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  tvmaze.getByQuery("suits", true, ['nextepisode'], function(result){
+      res.send(result)
+  })
 });
 
 module.exports = router;
