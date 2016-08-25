b12d0924b5b24e7f4f3e6b63972adf0097cae62f
Dylan <email": null>
2016-08-25 15:16:03 +1200

    not linking to login.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
-    one()
     clickHandler()
 }
 
-function one () {
+function clickHandler () {
   var one = document.getElementById('form1') //this allows to click anywhere
   one.addEventListener('click', searchSubmit)
 }
@@ -13,19 +12,18 @@ function one () {
 function searchSubmit () {
 
         alert("searchSubmit called")
-             return true
 
-  // evt.target.style.backgroundColor = 'blue'
+
 }
 
-function clickHandler () {
-  var sea = document.getElementById('input1') //this allows to click anywhere
-  sea.addEventListener('click', clickSubmit)
-}
-function clickSubmit () {
-
-        alert("clickSubmit called")
-             return true
-
-  // evt.target.style.backgroundColor = 'blue'
-}
+// function clickHandler () {
+//   var sea = document.getElementById('input1') //this allows to click anywhere
+//   sea.addEventListener('click', clickSubmit)
+// }
+// function clickSubmit () {
+//
+//         alert("clickSubmit called")
+//              return true
+//
+//   // evt.target.style.backgroundColor = 'blue'
+// }
