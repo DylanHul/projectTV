fb377a39148996745a56689d5d7929517b768e74
Dylan <email": null>
2016-08-25 14:05:05 +1200

    click handler added and working on submit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,23 +2,30 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
     one()
+    clickHandler()
 }
 
 function one () {
-  var one = document.getElementById('form1')
+  var one = document.getElementById('form1') //this allows to click anywhere
   one.addEventListener('click', searchSubmit)
 }
 
-function searchSubmit (evt) {
-  evt.target.style.backgroundColor = 'blue'
+function searchSubmit () {
+
+        alert("searchSubmit called")
+             return true
+
+  // evt.target.style.backgroundColor = 'blue'
 }
 
-// function clickHandler () {
-//
-//   clickHandler.addEventListener('click', function() {
-//     document.getElementById('btn').onclick = function onClick() {
-//            alert("onClick called")
-//            return true
-//        }
-//   })
-// }
+function clickHandler () {
+  var sea = document.getElementById('input1') //this allows to click anywhere
+  sea.addEventListener('click', clickSubmit)
+}
+function clickSubmit () {
+
+        alert("clickSubmit called")
+             return true
+
+  // evt.target.style.backgroundColor = 'blue'
+}
